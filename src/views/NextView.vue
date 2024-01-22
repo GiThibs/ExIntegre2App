f3a95eb6b5e9a32e2b4d9597cc61175cf62909a9
GiThibs <adelethibaut@gmail.com>
2024-01-22 14:48:37 +0100

    add files to public, add btn for interval & reset

--- src/views/NextView.vue
+++ src/views/NextView.vue
@@ -6,17 +6,22 @@
     <h3 class="actuel">Vous en êtes ici :<br>Saison {{ saison }} - Semaine {{ week }} - Session {{ day }}</h3><hr>
     <section class="progress">
       <div class="progressionbar">Barre de progression</div>
-      <div class="progressionstep">Progression de étapes : 3/{{ steps.length }}</div>
-      <div class="tempsecoule">Temps écoulé : 5/35 min</div>
+      <div class="tempsecoule">Temps écoulé : 5/{{ totalTimeMin }} min</div>
     </section>
+    <div class="btns">
+      <button class="startbtn">Démarrer la session ?</button>
+      <button class="stopbtn hidden">Arrêter la session ?</button>
+    </div>
       <table class="steparray">
-        <caption>
-          Ma sessions du jour
-        </caption>
+        <tr class="row">
+          <th>Etapes</th>
+          <th>Rythme</th>
+          <th>Temps en minutes</th>
+        </tr>
         <tr class="row" v-for="(step, key) in steps" :key="key" :index="key">
           <th>Etape n°{{ step.order }}</th>
           <th class="label">{{ step.label }}</th>
-          <th>{{ step.time / 60 }} min</th>
+          <th class="timeStep">{{ step.time / 60 }}</th>
         </tr>
       </table>
     <button class="resetbtn">Recommencer la session ?</button>
@@ -27,7 +32,7 @@
 <script setup>
 import { useRoute } from 'vue-router'
 import { usePlanningStore } from '@/stores/planning'
-import { onMounted } from 'vue';
+import { onMounted, ref } from 'vue';
 
 const route = useRoute()
 
@@ -42,6 +47,9 @@ const activeSession = saisons.find((el) => el.order == saison).semaines.find((el
 
 const steps = activeSession.etapes
 
+let totalTimeMin = ref(0)
+let totalTime = ref(0)
+
 onMounted(() => {
   const labels = document.querySelectorAll('.label')
   labels.forEach((el) => {
@@ -55,7 +63,60 @@ onMounted(() => {
       el.classList.add('echauff')
     }
   })
+
+  const allTime = document.querySelectorAll('.timeStep')
+  allTime.forEach((el) => {
+    totalTimeMin.value = totalTimeMin.value + Number.parseFloat(el.innerHTML)
+  })
+
+  totalTime.value = totalTimeMin.value * 60
+  console.log(totalTime.value)
+
+  const startBtn = document.querySelector('.startbtn')
+  const stopBtn = document.querySelector('.stopbtn')
+  const resetBtn = document.querySelector('.resetbtn')
+
+  let intervalStarted = null
+  const timer = ref(0)
+    
+  startBtn.addEventListener('click', e => {
+    startBtn.classList.add('hidden')
+    stopBtn.classList.remove('hidden')
+    if (!intervalStarted) {
+          console.log("session démarrée.")
+          intervalStarted = setInterval(() => {
+            timer.value++
+            console.log(timer.value)
+            if (timer.value == 5) {
+              console.log('Ca fait 5s')
+            }
+          }, 1000);
+          }
+    })
+  stopBtn.addEventListener('click', e => {
+    if(confirm('Voulez vous vraiment arrêter la session ? Vous devrez la recommencer du début !')) {
+      stopBtn.classList.add('hidden')
+      startBtn.classList.remove('hidden')
+      if (intervalStarted) {
+        clearInterval(intervalStarted)
+        intervalStarted = null
+        timer.value = 0
+        console.log("session arrêtée")
+      }
+    }
+    })
+  resetBtn.addEventListener('click', e => {
+    if (activeSession.done) {
+      if (confirm('Voulez vous vraiment réinitialiser cette session ? Elle ne sera plus considérée comme terminée !')) {
+        activeSession.done = false
+      }
+    }
+  })
 })
+
+
+
+
 
 </script>
 
@@ -64,6 +125,7 @@ onMounted(() => {
 
 table {
   border-spacing: 0;
+  margin-block: 1rem;
 }
 table > tr > th {
   padding: .3rem;
@@ -78,19 +140,30 @@ table > tr > th {
 }
 .progress {
   display: flex;
+  padding-block: 1rem;
   justify-content: space-between;
 }
 .echauff{
-  background-color: gold;
+  background-color: teal;
 }
 .marche {
   background-color: aqua;
 }
 .trot {
-  background-color: green;
+  background-color: cornflowerblue;
 }
 
-.resetbtn {
+.startbtn {
+  padding: 1rem;
+  background-color: green;
+  color: white;
+  border: none;
+  font-weight: bold;
+  text-transform: capitalize;
+  font-size: 1rem;
+  width: 100%;
+}
+.resetbtn, .stopbtn {
   padding: 1rem;
   background-color: red;
   color: white;
@@ -98,6 +171,7 @@ table > tr > th {
   font-weight: bold;
   text-transform: capitalize;
   font-size: 1rem;
+  width: 100%;
 }
 
 </style>
